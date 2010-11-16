34426987fcf243ee65244e4a6a6be056333b385d
Corin Langosch <info@netskin.com>
2010-11-16 00:25:59 +0000

    fix test to correctly handle the result of: assuming a bad network with a fake server should handle malformed response.

--- test/test_network.rb
+++ test/test_network.rb
@@ -27,7 +27,7 @@ class TestNetwork < Test::Unit::TestCase
 
       should 'handle malformed response' do
         memcached_mock(lambda {|sock| sock.write('123') }) do
-          assert_error Dalli::NetworkError, /EOFError/ do
+          assert_error Dalli::NetworkError, /Connection reset|EOFError/ do
             dc = Dalli::Client.new('localhost:19123')
             dc.get('abc')
           end
