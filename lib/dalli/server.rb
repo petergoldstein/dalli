3a8dd45959156078749942cd150cfb458ca67e68
Mike Perham <mperham@gmail.com>
2011-06-20 20:11:48 -0700

    Catch and handle write timeouts, fixes GH-99

--- lib/dalli/server.rb
+++ lib/dalli/server.rb
@@ -362,7 +362,7 @@ module Dalli
     def write(bytes)
       begin
         @sock.write(bytes)
-      rescue SystemCallError
+      rescue SystemCallError, Timeout::Error
         failure!
         retry
       end
