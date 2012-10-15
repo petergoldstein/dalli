577136e5570a8e9b9ff17440503435e3423b29e4
Brian Lopez <seniorlopez@gmail.com>
2012-10-15 14:07:04 -0700

    ensure serializer was set

--- test/test_serializer.rb
+++ test/test_serializer.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 require 'helper'
+require 'json'
 require 'memcached_mock'
 
 describe 'Serializer' do
@@ -12,12 +13,11 @@ describe 'Serializer' do
     original_serializer = Dalli.serializer
     begin
       Dalli.serializer = JSON
+      assert_equal JSON, Dalli.serializer
 
       memcached do |dc|
         assert dc.set("json_test", {"foo" => "bar"})
-        json = dc.get("json_test", :raw => true)
-        obj = JSON.parse(json)
-        assert_equal({"foo" => "bar"})
+        assert_equal({"foo" => "bar"}, dc.get("json_test"))
       end
     ensure
       Dalli.serializer = original_serializer
