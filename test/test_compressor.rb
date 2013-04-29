cf17e8cc90d9bbc591f530cd47d93807fd37856a
dfens <mikolajewskip@gmail.com>
2013-04-29 17:51:28 +0200

    Fix tests when dalli without default memcached running. Fixes #336.

--- test/test_compressor.rb
+++ test/test_compressor.rb
@@ -16,20 +16,24 @@ end
 describe 'Compressor' do
 
   should 'default to Dalli::Compressor' do
-    memcache = Dalli::Client.new('127.0.0.1:11211')
-    memcache.set 1,2
-    assert_equal Dalli::Compressor, memcache.instance_variable_get('@ring').servers.first.compressor
+    memcached_kill(29199) do |dc|
+      memcache = Dalli::Client.new('127.0.0.1:29199')
+      memcache.set 1,2
+      assert_equal Dalli::Compressor, memcache.instance_variable_get('@ring').servers.first.compressor
+    end
   end
 
   should 'support a custom compressor' do
-    memcache = Dalli::Client.new('127.0.0.1:11211', :compressor => NoopCompressor)
-    memcache.set 1,2
-    begin
-      assert_equal NoopCompressor, memcache.instance_variable_get('@ring').servers.first.compressor
+    memcached_kill(29199) do |dc|
+      memcache = Dalli::Client.new('127.0.0.1:29199', :compressor => NoopCompressor)
+      memcache.set 1,2
+      begin
+        assert_equal NoopCompressor, memcache.instance_variable_get('@ring').servers.first.compressor
 
-      memcached(19127) do |dc|
-        assert dc.set("string-test", "a test string")
-        assert_equal("a test string", dc.get("string-test"))
+        memcached(19127) do |dc|
+          assert dc.set("string-test", "a test string")
+          assert_equal("a test string", dc.get("string-test"))
+        end
       end
     end
   end
--- test/test_serializer.rb
+++ test/test_serializer.rb
@@ -6,20 +6,24 @@ require 'memcached_mock'
 describe 'Serializer' do
 
   should 'default to Marshal' do
-    memcache = Dalli::Client.new('127.0.0.1:11211')
-    memcache.set 1,2
-    assert_equal Marshal, memcache.instance_variable_get('@ring').servers.first.serializer
+    memcached_kill(29198) do |dc|
+      memcache = Dalli::Client.new('127.0.0.1:29198')
+      memcache.set 1,2
+      assert_equal Marshal, memcache.instance_variable_get('@ring').servers.first.serializer
+    end
   end
 
   should 'support a custom serializer' do
-    memcache = Dalli::Client.new('127.0.0.1:11211', :serializer => JSON)
-    memcache.set 1,2
-    begin
-      assert_equal JSON, memcache.instance_variable_get('@ring').servers.first.serializer
+    memcached_kill(29198) do |dc|
+      memcache = Dalli::Client.new('127.0.0.1:29198', :serializer => JSON)
+      memcache.set 1,2
+      begin
+        assert_equal JSON, memcache.instance_variable_get('@ring').servers.first.serializer
 
-      memcached(19128) do |dc|
-        assert dc.set("json_test", {"foo" => "bar"})
-        assert_equal({"foo" => "bar"}, dc.get("json_test"))
+        memcached(19128) do |dc|
+          assert dc.set("json_test", {"foo" => "bar"})
+          assert_equal({"foo" => "bar"}, dc.get("json_test"))
+        end
       end
     end
   end
