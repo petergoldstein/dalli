bd2849103fd7e1eaaa7cfaf4087a8cbf7a6659a5
Arthur Neves <arthurnn@gmail.com>
2014-01-09 16:10:18 -0500

    Return false if session_id is nil

--- lib/rack/session/dalli.rb
+++ lib/rack/session/dalli.rb
@@ -39,6 +39,7 @@ module Rack
       end
 
       def set_session(env, session_id, new_session, options)
+        return false unless session_id
         expiry = options[:expire_after]
         expiry = expiry.nil? ? 0 : expiry + 1
 
