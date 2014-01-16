d4f05f982bb6b3fdf62ba22b10d26e9a8dc7705d
Donald Plummer <donald.plummer@gmail.com>
2014-01-16 10:12:54 -0800

    Add ipv6 address support

--- test/test_server.rb
+++ test/test_server.rb
@@ -27,6 +27,43 @@ describe Dalli::Server do
     it 'handles ipv4 addresses' do
       s = Dalli::Server.new('127.0.0.1')
       assert_equal '127.0.0.1', s.hostname
+      assert_equal 11211, s.port
+      assert_equal 1, s.weight
+    end
+
+    it 'handles ipv6 addresses' do
+      s = Dalli::Server.new('[::1]')
+      assert_equal '::1', s.hostname
+      assert_equal 11211, s.port
+      assert_equal 1, s.weight
+    end
+
+    it 'handles ipv6 addresses with port' do
+      s = Dalli::Server.new('[::1]:11212')
+      assert_equal '::1', s.hostname
+      assert_equal 11212, s.port
+      assert_equal 1, s.weight
+    end
+
+    it 'handles ipv6 addresses with port and weight' do
+      s = Dalli::Server.new('[::1]:11212:2')
+      assert_equal '::1', s.hostname
+      assert_equal 11212, s.port
+      assert_equal 2, s.weight
+    end
+
+    it 'handles a FQDN' do
+      s = Dalli::Server.new('my.fqdn.com')
+      assert_equal 'my.fqdn.com', s.hostname
+      assert_equal 11211, s.port
+      assert_equal 1, s.weight
+    end
+
+    it 'handles a FQDN with port and weight' do
+      s = Dalli::Server.new('my.fqdn.com:11212:2')
+      assert_equal 'my.fqdn.com', s.hostname
+      assert_equal 11212, s.port
+      assert_equal 2, s.weight
     end
   end
 end
