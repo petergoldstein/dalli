5f03f9efa273296903e7a1fd2d0046eb7f9f833a
Peter Goldstein <peter@valimail.com>
2020-12-26 14:28:08 -0800

    Explicitly install the SASL dev

--- scripts/install_memcached.sh
+++ scripts/install_memcached.sh
@@ -3,8 +3,9 @@
 
 sudo apt-get -y remove memcached
 sudo apt-get install libevent-dev
-wget https://memcached.org/files/memcached-1.5.20.tar.gz
-tar -zxvf memcached-1.5.20.tar.gz
-cd memcached-1.5.20
+sudo apt-get install libsasl2-dev
+wget https://memcached.org/files/memcached-1.5.22.tar.gz
+tar -zxvf memcached-1.5.22.tar.gz
+cd memcached-1.5.22
 ./configure --enable-sasl
 make
