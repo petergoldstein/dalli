ae70ae972924dc62b75b8d6a69d9f9fed68c7c12
Brandon Dunne <brandondunne@hotmail.com>
2021-08-31 15:58:59 -0400

    Put memcached executable on the PATH

--- scripts/install_memcached.sh
+++ scripts/install_memcached.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 sudo apt-get -y remove memcached
 sudo apt-get install libevent-dev
 sudo apt-get install libsasl2-dev
@@ -9,3 +8,4 @@ tar -zxvf memcached-1.5.22.tar.gz
 cd memcached-1.5.22
 ./configure --enable-sasl
 make
+sudo mv memcached /usr/local/bin/
