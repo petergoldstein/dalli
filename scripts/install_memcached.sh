9b3769329631695d6a46e2bb86d9759bd76c3fd1
Peter Goldstein <peter@Peters-MacBook-Pro.local>
2021-10-01 09:18:29 -0700

    Bump memcached version

--- scripts/install_memcached.sh
+++ scripts/install_memcached.sh
@@ -3,9 +3,9 @@
 sudo apt-get -y remove memcached
 sudo apt-get install libevent-dev
 sudo apt-get install libsasl2-dev
-wget https://memcached.org/files/memcached-1.5.22.tar.gz
-tar -zxvf memcached-1.5.22.tar.gz
-cd memcached-1.5.22
+wget https://memcached.org/files/memcached-1.6.9.tar.gz
+tar -zxvf memcached-1.6.9.tar.gz
+cd memcached-1.6.9
 ./configure --enable-sasl
 make
 sudo mv memcached /usr/local/bin/
