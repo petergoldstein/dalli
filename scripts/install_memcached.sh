bdd96c008cf410e99861435f9c64aac924ee203e
Peter Goldstein <peter.m.goldstein@gmail.com>
2021-10-19 13:19:16 -0700

    Pull the Memcached version out into a variable

--- scripts/install_memcached.sh
+++ scripts/install_memcached.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+version="1.5.22"
+
 sudo apt-get -y remove memcached
 sudo apt-get install libevent-dev
 sudo apt-get install libsasl2-dev
-wget https://memcached.org/files/memcached-1.5.22.tar.gz
-tar -zxvf memcached-1.5.22.tar.gz
-cd memcached-1.5.22
+wget https://memcached.org/files/memcached-${version}.tar.gz
+tar -zxvf memcached-${version}.tar.gz
+cd memcached-${version}
 ./configure --enable-sasl --enable-tls
 make
 sudo mv memcached /usr/local/bin/
