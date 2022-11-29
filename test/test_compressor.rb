bcfe112e47a2769f63b068b26615ff6dbe800754
Jean byroot Boussier <jean.boussier+github@shopify.com>
2022-11-29 15:46:17 +0100

    Pass Encoding instances rather than encoding names as String (#939)
    This doesn't really matter much in the grand scheme of things
    but is a pet peeve of mine. It's very marginally faster as
    Ruby doesn't have to search for that name in the encoding table.
    
    Co-authored-by: Jean Boussier <jean.boussier@gmail.com>

--- test/test_compressor.rb
+++ test/test_compressor.rb
@@ -4,10 +4,9 @@ require_relative 'helper'
 
 describe 'Dalli::Compressor' do
   it 'compresses data using Zlib::Deflate' do
-    assert_equal (+"x\x9CKLJN\x01\x00\x03\xD8\x01\x8B").force_encoding('ASCII-8BIT'),
+    assert_equal "x\x9CKLJN\x01\x00\x03\xD8\x01\x8B".b,
                  ::Dalli::Compressor.compress('abcd')
-    assert_equal (+"x\x9C+\xC9HU(,\xCDL\xCEVH*\xCA/\xCFSH\xCB\xAFP\xC8*\xCD-(\x06\x00z\x06\t\x83")
-      .force_encoding('ASCII-8BIT'),
+    assert_equal "x\x9C+\xC9HU(,\xCDL\xCEVH*\xCA/\xCFSH\xCB\xAFP\xC8*\xCD-(\x06\x00z\x06\t\x83".b,
                  ::Dalli::Compressor.compress('the quick brown fox jumps')
   end
 
