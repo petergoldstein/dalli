089c83635053b780dc4ac8e3f7056c5bf57f7026
Matt Larraz <mlarraz@users.noreply.github.com>
2025-04-01 16:22:11 -0400

    Fix Rubocop

--- lib/dalli/protocol/value_serializer.rb
+++ lib/dalli/protocol/value_serializer.rb
@@ -23,7 +23,7 @@ module Dalli
 
       def initialize(protocol_options)
         @serialization_options =
-          DEFAULTS.merge(protocol_options.select { |k, _| OPTIONS.include?(k) })
+          DEFAULTS.merge(protocol_options.slice(*OPTIONS))
       end
 
       def store(value, req_options, bitflags)
@@ -34,7 +34,7 @@ module Dalli
       end
 
       def retrieve(value, bitflags)
-        serialized = (bitflags & FLAG_SERIALIZED) != 0
+        serialized = bitflags.anybits?(FLAG_SERIALIZED)
         if serialized
           begin
             serializer.load(value)
